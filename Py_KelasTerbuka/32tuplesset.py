566b808a0e9489736f1e50ae4154ac5d0283c3b4
FJ00 <77333830+ali1248219@users.noreply.github.com>
2023-03-23 00:29:42 +0700

    tambahan belajar dictionary tengah malem

---
+++ Py_KelasTerbuka/32tuplesset.py
@@ -0,0 +1,17 @@
+# list, pakai kurung siku
+
+data_list = [1,2,3]
+data_list.append([2,3,4])
+print(data_list)
+
+# tuples, pakai kurung biasa
+# tuples tidak bisa diubah
+# data_tuples [0] =7
+# tuples tidak bisa diappend
+# fungsi tuples adalah untuk perhitungan yang tidak bisa diubah.
+data_tuples = (4,5,6)
+print(data_tuples[0])
+
+# sets, aksi yang tidak ada index, jadi ukurannya tidak ngaruh
+# print(data_set[1])
+data_set = {1,2,3,4,5}
