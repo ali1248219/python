0057e9cbb5d8765c6fc4b802a518d0194131fbb9
ali1248219 <alirelevan@gmail.com>
2023-03-23 21:01:18 +0700

    Test Fungsi

---
+++ Py_KelasTerbuka/38fungsi.py
@@ -0,0 +1,14 @@
+# fungsi , method atau rutin
+# fungsi pada python
+# DRY , Dont Repeat Yourself
+
+"""
+Membuat fungsi (def)
+"""
+
+
+def hello_world():
+    print("hello world")
+
+
+hello_world()
---
+++ Py_KelasTerbuka/39fungsiargument.py
@@ -0,0 +1,20 @@
+# fungsi dengan argumen
+# fungsi() adalah sebagai argument atau parameter atau input
+
+
+def hello_world(nama):
+    # badan fungsi
+    print(f"selamat datang dunia wahai {nama}")
+
+
+hello_world("FJ")
+
+# program tambah
+
+
+def tambah(angka_1, angka_2):
+    hasil = angka_1 + angka_2
+    print(f"{angka_1} ditambah {angka_2} = {hasil}")
+
+
+tambah(5, 3)
