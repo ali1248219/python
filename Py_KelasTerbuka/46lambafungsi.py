9145d4b86fcd34589d9fe04e4d184ed1d6a3379b
ali1248219 <alirelevan@gmail.com>
2023-03-29 17:32:15 +0700

    update dari kos

--- Py_KelasTerbuka/46lambafungsi.py
+++ Py_KelasTerbuka/46lambafungsi.py
@@ -2,86 +2,100 @@
 
 # membuat program lebih simpel
 
-def kuadrat (angka):
+def kuadrat(angka):
     return angka**2
+
 
 print(kuadrat(3))
 
 # kita coba dengan lambda
 # output = lambda argument:expression
-lambda_kuadrat = lambda angka:angka**2
+
+
+def lambda_kuadrat(angka): return angka**2
+
 
 print(lambda_kuadrat(3))
 
-lambda_2_input = lambda num,pow : num**pow
-print(lambda_2_input(4,2))
+
+def lambda_2_input(num, pow): return num**pow
+
+
+print(lambda_2_input(4, 2))
 
 # kegunaan
 # membuat sorting list
-data_list = ["otong","dudung","ucup"]
+data_list = ["otong", "dudung", "ucup"]
 data_list.sort()
 print(data_list)
 
 # sortin berdasarkan jumlah huruf pakai fungsi
-data_list = ["otong","dudung","ucup"]
+data_list = ["otong", "dudung", "ucup"]
+
+
 def panjang_nama(nama):
     return len(nama)
+
 
 data_list.sort(key=panjang_nama)
 print(data_list)
 
 
-data_list = ["otong","dudung","ucup"]
+data_list = ["otong", "dudung", "ucup"]
 data_list.sort(key=len)
-print (data_list)
+print(data_list)
 
 
 # sortin berdasarkan jumlah huruf pakai lambda
 
-data_list = ["otong","dudung","ucup"]
-data_list.sort(key=lambda nama:len(nama))
+data_list = ["otong", "dudung", "ucup"]
+data_list.sort(key=lambda nama: len(nama))
 
 # filter
 
-data_angka = [1,5,9,4,6,8,2,7,3]
+data_angka = [1, 5, 9, 4, 6, 8, 2, 7, 3]
 data_angka.sort()
 print(data_angka)
+
 
 def angka_kurang_dari_lima(angka):
     return angka < 5
 
-data_angka_baru = list(filter(angka_kurang_dari_lima,data_angka))
+
+data_angka_baru = list(filter(angka_kurang_dari_lima, data_angka))
 print(data_angka_baru)
 
-data_angka_baru = list(filter(lambda x:x<5,data_angka))
+data_angka_baru = list(filter(lambda x: x < 5, data_angka))
 print(data_angka_baru)
 
 # data genap
-data_genap = list(filter(lambda x:(x%2==0),data_angka))
+data_genap = list(filter(lambda x: (x % 2 == 0), data_angka))
 print(data_genap)
 
 # data ganjil
-data_ganjil = list(filter(lambda x:(x%2!=0),data_angka))
+data_ganjil = list(filter(lambda x: (x % 2 != 0), data_angka))
 print(data_ganjil)
 
 # data kelipatan 3
-data_3 = list(filter(lambda x:(x%3==0),data_angka))
+data_3 = list(filter(lambda x: (x % 3 == 0), data_angka))
 print(data_3)
 
 # anonymous function
 # currying <- haskell curry
 
-def pangkat (angka,n):
+
+def pangkat(angka, n):
     hasil = angka**n
     return hasil
 
-data_hasil = pangkat(3,2)
-print (data_hasil)
 
+data_hasil = pangkat(3, 2)
+print(data_hasil)
 
 
 def pangkat(n):
-    return lambda angka:angka**n
+    return lambda angka: angka**n
+
 
 pangkat4 = pangkat(4)
 print(f"hasil pangkat4 dari angka 2 adalah {pangkat4(2)} ")
--- Py_KelasTerbuka/47localglobalscope.py
+++ Py_KelasTerbuka/47localglobalscope.py
@@ -1,1 +1,63 @@
 # local dan global scope variable
+
+nama_global = "dudung"  # akan hidup di manapun
+
+
+# akses variabel global dalam loop
+
+def fungsi():
+    print(f"fungsi menampilkan {nama_global}")
+
+
+fungsi()
+
+for i in range(0, 5):
+    print(f"loop {i} = {nama_global}")
+
+
+# variabel lokal scope
+def fungsi2():
+    nama_lokal = "ucup"  # ini bisa diakses hanya di fungsi ini saja
+
+# var tidak akan bisa diakses karena posisinya di global
+# fungsi2()
+# print(nama_local)
+
+# contoh penggunaan
+# urutan variabel haruslah di atas fungsi dll
+
+
+nama = "otong"
+
+
+def say_otong():
+    print(f"hai teman teman {nama}")
+
+
+say_otong()
+
+# merubah variabel global
+
+angka = 0
+
+
+def ubah_angka(nilai_baru):
+    # kita deklarasikan dulu variabel untuk emnjadi global, hanya berlaku untuk fungsi
+    global angka
+    angka = nilai_baru
+
+
+print(angka)
+ubah_angka(10)
+print(angka)
+
+tinggi = 10
+
+
+def ubah_tinggi(nilai_tinggi_baru):
+    global tinggi
+    tinggi = nilai_tinggi_baru
+
+
+ubah_tinggi(12)
+print(tinggi)
